5bae649eb9aba0cf570753bb4b404d57e503dcf5
afmorac <andresfmora.catro@gmail.com>
2025-05-22 01:15:06 -0400

    0

---
+++ listas.py
@@ -0,0 +1,22 @@
+""""
+mixta = [1,'fruta',['a', 'b']]
+print(mixta)
+print(mixta[-1])
+mixta[2] = 'a'
+print(mixta)
+
+nums = [1,2,3,4,5]
+print(nums[1:4])
+
+#print((mixta + nums) *2)
+
+nums.append(9)
+print(nums)
+
+del nums[2]
+print(nums[2])
+print(nums)
+"""
+a = [1,2,3,4]
+
+print(a[-3])
