5b1b008da2748c9eccc9462475757576e7771f83
afmorac <andresfmora.catro@gmail.com>
2025-05-25 15:33:19 -0400

    0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Este módulo define una clase vacía llamada Square. 
+"""Este módulo define una clase vacía llamada Square.
 Es el punto de partida pa' después meterle cosas más útiles.
 """
 
+
 class Square:
-    """Clase que representa un cuadrado. Ahora mismo está vacía, 
+    """Clase que representa un cuadrado. Ahora mismo está vacía,
     pero ya mismito le vamos a dar vida con más funciones.
     """
     pass
