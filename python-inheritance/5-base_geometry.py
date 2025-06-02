dd4e28c8ce2536d569b76dffa53f06b46e3f1f91
afmorac <andresfmora.catro@gmail.com>
2025-06-01 23:07:47 -0400

    5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Clase vacía llamada BaseGeometry, por ahora no hace na.
+"""
+
+
+class BaseGeometry:
+    """
+    Clase base de geometría, aún sin definición ni métodos.
+    """
+    pass
