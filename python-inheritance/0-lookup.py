7d2daf58e1aa78c36a097b1c969559b557bdbd4c
afmorac <andresfmora.catro@gmail.com>
2025-06-01 22:47:20 -0400

    0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Este modulo tiene una funcion q te deja ver todo lo q tiene un objeto por dentro
+como sus metodos y atributos disponibles, aunque algunos ni se usan.
+"""
+
+def lookup(obj):
+    """
+    Devuelve una lista de atributos y metodos disponibles pa un objeto.
+
+    Args:
+        obj: El objeto q vamos a mirar por dentro
+
+    Returns:
+        list: Una lista (tipo lista normal) con todos los nombres de metodos y cosas q tiene
+    """
+    return dir(obj)  # esto usa dir, q es como un scanner pa ver todo lo q hay dentro del obj
