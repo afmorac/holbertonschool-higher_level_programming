5000979db3ebf073d50b6216258dc31dcda53d0e
afmorac <andresfmora.catro@gmail.com>
2025-06-01 23:08:51 -0400

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Clase base de geometría con método area() sin implementar
+"""
+
+
+class BaseGeometry:
+    """
+    Clase base de geometría, por ahora solo tiene area como placeholder
+    """
+
+    def area(self):
+        """
+        Método que debería calcular el área, pero aún no está implementado
+        """
+        raise Exception("area() is not implemented")
