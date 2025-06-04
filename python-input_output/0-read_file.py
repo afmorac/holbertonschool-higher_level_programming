fcca70fe978815ef62a0ea69a20d507f95b70d14
afmorac <andresfmora.catro@gmail.com>
2025-06-03 23:59:17 -0400

    0.2

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,8 @@ Func. que lee e imprime un archivo
 """
 
 def read_file(filename=""):
+    """
+    Lectura de archivo de texto y lo muestra en stdout
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
