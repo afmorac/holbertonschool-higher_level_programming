0941d243a56138b33b6a5753499d76bd7a81d44d
afmorac <andresfmora.catro@gmail.com>
2025-06-03 23:53:06 -0400

    0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/urs/bin/python3
+"""
+Func. que lee e imprime un archivo
+"""
+
+def read_file(filename=""):
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
