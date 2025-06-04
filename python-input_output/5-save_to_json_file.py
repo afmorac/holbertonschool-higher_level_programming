0b650402cb41040a361139117c05f186c3180b90
afmorac <andresfmora.catro@gmail.com>
2025-06-04 02:11:45 -0400

    5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Mod. define la funcion save_to_json_file
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Guarda un obj en un archivo usando JSON
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dumps(my_obj, f)
