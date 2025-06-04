5f853afd5076fe85dcfaa69fd3e3c8b4d01bea4c
afmorac <andresfmora.catro@gmail.com>
2025-06-04 00:51:45 -0400

    1.2

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,12 +2,12 @@
 """
 Mod. define la funcion write_file
 """
-    
-    
+
+
 def write_file(filename="", text=""):
-        """
-        Func. escribir cadena a txt file 
-        retornar numero de caracteres
-        """
-        with open(filename, "w", encoding="utf-8") as f:
-            return f.write(text)
+    """
+    Func. escribir cadena a txt file 
+    retornar numero de caracteres
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
