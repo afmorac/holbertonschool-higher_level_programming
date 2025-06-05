2aee8a886f9d9237c78654370ed33f57f861344e
afmorac <andresfmora.catro@gmail.com>
2025-06-05 11:23:45 -0400

    11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Doc
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+"""
+funcion que permite decidir que atributos
+incluir
+"""
+
+
+def to_json(self, attrs=None):
+    if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
+        return {
+            key: getattr(self, key)
+            for key in attrs
+            if hasattr(self, key)
+        }
+    return self.__dict__
+
+
+def reload_from_json(self, json):
+    for key, value in json.items():
+        setattr(self, key, value)
