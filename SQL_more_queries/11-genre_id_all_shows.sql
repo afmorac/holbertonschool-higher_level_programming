6e9761812ad6d5f610210d385b4a77b0a41c8da7
afmorac <andresfmora.catro@gmail.com>
2025-06-29 23:10:25 -0400

    11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- Script para mostrar todos los shows y su genre_id (si tiene)
+-- Si no tiene género, muestra NULL
+-- Ordenado por título del show y genre_id
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
