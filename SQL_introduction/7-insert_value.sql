892be7505874ad2b3d768c4f07f891329fcba58a
afmorac <andresfmora.catro@gmail.com>
2025-06-29 20:57:16 -0400

    7

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Script para añadir una fila nueva a la tabla first_table
+-- Se insertan los valores: id = 89 y name = 'Best School'
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
