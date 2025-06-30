809e4ed01937be8ffcee094100d3df0b2d9ff9d1
afmorac <andresfmora.catro@gmail.com>
2025-06-29 21:02:47 -0400

    10

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Script para mostrar score y name de second_table ordenado por el mejor score primero
+-- Ordena de mayor a menor usando ORDER BY DESC
+SELECT score, name FROM second_table ORDER BY score DESC;
