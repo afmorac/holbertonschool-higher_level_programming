dd7fffbb07ee725730795bde7bacba8c96db27cf
afmorac <andresfmora.catro@gmail.com>
2025-06-29 22:47:56 -0400

    0

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Script para mostrar los privilegios de user_0d_1 y user_0d_2
+-- OJO: Si alguno no existe, da error
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
