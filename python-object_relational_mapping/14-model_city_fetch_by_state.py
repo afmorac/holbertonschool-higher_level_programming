136c19bd411e9f42db98a875c10dc77dac4c9811
afmorac <andresfmora.catro@gmail.com>
2025-06-29 23:51:07 -0400

    14

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Imprime todas las ciudades con su estado asociado desde la base de datos
+"""
+
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # Crear engine y sesión
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Consulta con join: ciudad + estado
+    results = session.query(City, State).join(State).order_by(City.id).all()
+
+    for city, state in results:
+        print(f"{state.name}: ({city.id}) {city.name}")
