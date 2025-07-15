3b13c5761f6f064c30f13aecb8deddd5d1ec5024
afmorac <andresfmora.catro@gmail.com>
2025-07-15 11:14:44 -0400

    2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argsCnt = process.argv.length - 2;
+
+if (argsCnt === 0) {
+    console.log('No argument');
+} else if (argsCnt === 1) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
